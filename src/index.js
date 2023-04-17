130b1a5d5d8e3ac79b814439dea999cdda009dbe
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-04-17 19:02:12 +0300

    commit fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/goit-react-hw-08-phone">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
